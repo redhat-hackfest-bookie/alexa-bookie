cdd69619f36ac24cc2d2aeab4c0de00059aa68b2
Marcelo Ohashi <mohashi@gmail.com>
2020-04-07 13:44:52 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# hackfest-alexa-bookie project
+
+## Run locally
+
+```shell
+mvn clean quarkus:dev
+```
---
+++ src/test/java/com/redhat/hackfest/NativeBackendResurceIT.java
@@ -0,0 +1,9 @@
+package com.redhat.hackfest;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeBackendResurceIT extends BackendResurceTest {
+
+    // Execute the same tests but in native mode.
+}
