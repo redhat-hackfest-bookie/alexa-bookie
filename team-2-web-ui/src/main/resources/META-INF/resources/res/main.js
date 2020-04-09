6e5462d07395d0d4e96b349cba0a99049bd7726c
Greg John Stewart <contact@gjstewart.net>
2020-04-08 23:52:31 -0400

    Added stats page and spinner to show loading on bookie section.

--- team-2-web-ui/src/main/resources/META-INF/resources/res/main.js
+++ team-2-web-ui/src/main/resources/META-INF/resources/res/main.js
@@ -1,8 +1,8 @@
-
 var sportListLocation = "/res/sports.json";
-var serviceEndpoint = "http://hackfest-alexa-bookie-alexa.apps.cluster-fed2.fed2.example.opentlc.com/api/v1/bookie/";
+var bookieServiceEndpoint = "http://hackfest-alexa-bookie.alexa.apps.cluster-fed2.fed2.example.opentlc.com/api/v1/bookie/";
+var statsServiceEndpoint = "http://spring-data-jpa-quickstart-alexa.apps.cluster-fed2.fed2.example.opentlc.com/";
 
-
+var bookieContentJS = document.getElementById("bookieContent");
 var sportSelect = $('#sportSelect');
 var homeTeamSelect = $('#homeTeamSelect');
 var awayTeamSelect = $('#awayTeamSelect');
@@ -12,12 +12,46 @@ var getPredictionButton = $('#getPredictionButton');
 var predictionContainer = $('#predictionContainer');
 var predictionText = $('#predictionText');
 
+var totalRequestsSpan = $('#totalRequestsSpan');
+var requestsPerSportPercentageDiv = $('#requestsPerSportPercentageDiv');
+var requestsPerSportTableBody = $('#requestsPerSportTableBody');
+
+var lastReloadFinished = true;
+
+var spinnerOpts = {
+  lines: 13, // The number of lines to draw
+  length: 38, // The length of each line
+  width: 17, // The line thickness
+  radius: 45, // The radius of the inner circle
+  scale: 1, // Scales overall size of the spinner
+  corners: 1, // Corner roundness (0..1)
+  color: '#000000', // CSS color or array of colors
+  fadeColor: 'transparent', // CSS color or array of colors
+  speed: 1, // Rounds per second
+  rotate: 0, // The rotation offset
+  animation: 'spinner-line-fade-quick', // The CSS animation name for the lines
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  className: 'spinner', // The CSS class to assign to the spinner
+  top: '50%', // Top position relative to parent
+  left: '50%', // Left position relative to parent
+  shadow: '0 0 1px transparent', // Box-shadow for the lines
+  position: 'absolute' // Element positioning
+};
+
+var spinner = new Spinner(spinnerOpts);
+
+
+
+var runningSpinner = null;
+
 console.log("Page loaded!");
 
 $(document).ready(function() {
 	
 	//read in sport list
 	jQuery.get(sportListLocation, function(data) {
+		startSpinner();
 		console.log("Read in sports data: " + data);	
 	
 		var sportsList = data.sports;
@@ -31,18 +65,33 @@ $(document).ready(function() {
         		text : sport 
     		}));
 		});
+		stopSpinner();
 	});
 	
 	sportSelect.change(function() {
+		startSpinner();
 		populateTeamSelects();
 	});
 	
 	getPredictionButton.click(function() {
+		startSpinner();
 		getBookiePrediction();
 	});
 	
+	$(function () {
+    	setInterval(reloadStatistics, 5000);
+    });
+    reloadStatistics();
+	
     console.log("ready!");
 });
+
+function stopSpinner(){
+	spinner.stop();
+}
+function startSpinner(){
+	spinner.spin(bookieContentJS);
+}
 
 
 function resetTeams(){
@@ -65,7 +114,7 @@ function populateTeamSelects(){
 	
 	console.log("Sport selected: \"" + selectedSport + "\"");
 	
-	makeCallToService(selectedSport, function(data, status){
+	makeCallToBookieService(selectedSport, function(data, status){
 		console.log("Got team data from service: " + data);
 		
 		data.forEach(function(team, index) {
@@ -81,6 +130,7 @@ function populateTeamSelects(){
         		text : teamName 
     		}));
 		});
+		stopSpinner();
 	});
 }
 
@@ -107,10 +157,15 @@ function getBookiePrediction(){
 		updatePredictionResult("No away team selected.");
 		return;
 	}
+	if(selectedHome == selectedAway){
+		console.log("Same team selected for home and away.");
+		updatePredictionResult("A team can't play against itself.");
+		return;
+	}
 	
 	var request = selectedSport + "/" + selectedHome + "/" + selectedAway;
 	
-	makeCallToService(request, function(response, status){
+	makeCallToBookieService(request, function(response, status){
 		console.log("Got game result: " + response);
 
 	    var textOut = "";
@@ -138,6 +193,7 @@ function getBookiePrediction(){
 	    }
 	    
 	    updatePredictionResult(textOut);
+	    stopSpinner();
 	});
 }
 
@@ -151,9 +207,67 @@ function updatePredictionResult(resultString){
 	predictionText.text(resultString);
 }
 
-function makeCallToService(request, process){
-	var fullEndpoint = serviceEndpoint + request;
-	console.log("Hitting endpoint: " + fullEndpoint);
+function makeCallToBookieService(request, process){
+	var fullEndpoint = encodeURI(bookieServiceEndpoint + request);
+	console.log("Hitting bookie endpoint: " + fullEndpoint);
+	
+	$.get(fullEndpoint, process);
+}
+
+function reloadStatistics(){
+	console.log("Reloading stats data.");
+	if(!lastReloadFinished){
+		console.log("Last reload not completed yet. Skipping reload.");
+		return;
+	}
+	lastReloadFinished = false;
+	
+	reloadSportStats();
+	
+	lastReloadFinished = true;
+}
+
+function reloadSportStats(){
+	makeCallToStatsService("sports", function(sports, status){
+		console.log("Got sports stats result: " + sports);
+		
+		var total = 0;
+		sports.forEach(function(sport, index) {
+			total += sport.hitCount;
+		});
+		
+		totalRequestsSpan.text(total);
+		
+		requestsPerSportTableBody.empty();
+		requestsPerSportPercentageDiv.empty();
+		var lastColor = cycleColor("");
+		sports.forEach(function(sport, index) {
+			var percentage = (((sport.hitCount/total) * 100).toFixed(3));
+			requestsPerSportTableBody.append('<tr><td>'+sport.name+'</td><td>'+sport.hitCount+'</td><td>'+percentage+'</td></tr>');
+			
+			requestsPerSportPercentageDiv.append('<div class="progress-bar '+lastColor+' requestsPerSportPercentageSegment" role="progressbar" style="width: '+percentage+'%;" aria-valuenow="'+percentage+'" aria-valuemin="0" aria-valuemax="100">'+sport.name+'</div>');
+			
+			lastColor = cycleColor(lastColor);
+		});
+	});
+}
+
+function cycleColor(last){
+	switch(last){
+		case "bg-success":
+			return "bg-info";
+		case "bg-info":
+			return "bg-warning";
+		case "bg-warning":
+			return "bg-danger";
+		default:
+			return "bg-success";
+	}
+}
+
+function makeCallToStatsService(request, process){
+	var fullEndpoint = encodeURI(statsServiceEndpoint + request);
+	console.log("Hitting stats endpoint: " + fullEndpoint);
 	
 	$.get(fullEndpoint, process);
 }
